0f95d603571de9fd8b250a814e3a3174259835ce
resmikumar <61494039+resmikumar@users.noreply.github.com>
2023-01-18 18:13:00 +0530

    1 file modified, 1 added, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,11 +1,126 @@
 import * as React from 'react';
-import './style.css';
+import './App.css';
 
-export default function App() {
+type ITodo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+type ITodos = {
+  todos: ITodo[];
+};
+
+function App() {
+  const [todos, setTodos] = React.useState<ITodos>({ todos: [] });
+  const addTodos = (title: string) => {
+    setTodos({
+      todos: [
+        { title, completed: false, id: todos.todos.length + 1 },
+        ...todos.todos,
+      ],
+    });
+  };
+  const deleteTodos = (id: number) => {
+    setTodos({
+      todos: todos.todos.filter((t) => t.id !== id),
+    });
+  };
+  const toggleTodos = (id: number) => {
+    setTodos({
+      todos: todos.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    });
+  };
+
   return (
-    <div>
-      <h1>Quiz</h1>
-      
+    <div className="App">
+      <AddTodoComponent addTodos={addTodos} />
+      <hr />
+      <TodosComponent
+        todos={todos}
+        toggleTodos={toggleTodos}
+        deleteTodos={deleteTodos}
+      />
     </div>
   );
 }
+
+const TodosComponent: React.FC<{
+  todos: ITodos;
+  toggleTodos: (id: number) => void;
+  deleteTodos: (id: number) => void;
+}> = ({ todos, toggleTodos, deleteTodos }) => {
+  const deleteTodo = (id: number) => {
+    if (window.confirm(`Are you sure you want to delete todo?`)) {
+      deleteTodos(id);
+    }
+  };
+  return (
+    <div className="section__todos">
+      <h2>Todos</h2>
+      {todos.todos.length ? (
+        <ul className="todos">
+          {todos.todos.map((todo) => (
+            <li key={todo.id}>
+              <span
+                style={{
+                  textDecoration: todo.completed ? 'line-through' : 'none',
+                }}
+              >
+                {todo.title}
+              </span>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => toggleTodos(todo.id)}
+              />
+              <button
+                onClick={() => {
+                  deleteTodo(todo.id);
+                }}
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>No Todo has been created</div>
+      )}
+    </div>
+  );
+};
+
+const AddTodoComponent = ({
+  addTodos,
+}: {
+  addTodos: (text: string) => void;
+}) => {
+  const [todo, setTodo] = React.useState<string>('');
+  const submit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if (!todo) {
+      alert('Please enter a todo');
+    } else {
+      addTodos(todo);
+      setTodo('');
+    }
+  };
+  return (
+    <div className="AddTodo">
+      <form>
+        <input
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button onClick={submit}>Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
